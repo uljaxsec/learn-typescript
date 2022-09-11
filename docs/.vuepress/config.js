e520b674ac8e8036333a154f5202c65191996918
uljaxsec <uljaxsec1991@163.com>
2022-09-11 09:25:12 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,43 @@
+module.exports = {
+    title: 'TypeScript4 文档',
+    // locales: {
+    //     '/': {
+    //         lang: 'zh-CN'
+    //     }
+    // },
+    description: 'TypeScript4 最新官方文档翻译',
+    base: '/learn-typescript/',
+    theme: 'reco',
+    themeConfig: {
+        nav: [
+            { text: '首页', link: '/' },
+            {
+                text: '冴羽的 JavaScript 博客',
+                items: [
+                    { text: 'Github', link: 'https://github.com/mqyqingfeng' },
+                    { text: '掘金', link: 'https://juejin.cn/user/712139234359182/posts' }
+                ]
+            }
+        ],
+        sidebar: [
+            {
+                title: '欢迎学习',
+                path: '/',
+                collapsable: false, // 不折叠
+                children: [
+                    { title: "学前必读", path: "/" }
+                ]
+            },
+            {
+                title: "基础学习",
+                path: '/handbook/ConditionalTypes',
+                collapsable: false, // 不折叠
+                children: [
+                    { title: "条件类型", path: "/handbook/ConditionalTypes" },
+                    { title: "泛型", path: "/handbook/Generics" }
+                ],
+            }
+        ],
+        subSidebar: 'auto'
+    }
+}
